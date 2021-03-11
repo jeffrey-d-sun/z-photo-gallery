bf736e1c88f2c819ac20d6905f491a135faa3fa3
jeffrey d sun <jeffreydsun90@gmail.com>
2021-03-10 20:20:39 -0800

    add hover-over sticky tool tip

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import MouseToolTip from 'react-sticky-mouse-tooltip'
 
 const Gallery = ({ pokemon, imgSrc }) => {
   const { stats, name, id } = pokemon
   const { height, weight, types } = stats
+  const [showToolTip, setToolTip] = useState(false)
 
   const capitalize = (str) => {
     return str[0].toUpperCase() + str.slice(1)
   }
 
   return (
-    <div className="gallery-container">
+    <div
+      className="gallery-container"
+      onMouseEnter={() => setToolTip(true)}
+      onMouseLeave={() => setToolTip(false)}
+    >
       <h2 className="pokemon-name" onClick={() => window.open(`https://pokemon.fandom.com/wiki/${name}`, "_blank")}>
         {capitalize(name)}
       </h2>
@@ -21,23 +27,28 @@ const Gallery = ({ pokemon, imgSrc }) => {
           onClick={() =>
             window.open(`https://pokemon.fandom.com/wiki/${name}`, "_blank")
           }
-          onMouseOver={() => {
-
-          }}
         />
-        <figcaption className="stats">
-          <div>Pokedex ID: {id}</div>
-          <div>
-            Height: {height}, Weight: {weight}
-          </div>
-          <div>
-            Types:{" "}
-            {types.map((type, index, arr) => (
-              <span>{type}{index !== arr.length - 1 ? ', ' : ''}</span>
-            ))}
-          </div>
-        </figcaption>
+          <hr />
+          <figcaption className="stats">
+            <div>Pokedex ID: {id}</div>
+            <div>
+              Height: {height}, Weight: {weight}
+            </div>
+            <div>
+              Types:{" "}
+              {types.map((type, index, arr) => (
+                <span>{type}{index !== arr.length - 1 ? ', ' : ''}</span>
+              ))}
+            </div>
+          </figcaption>
       </figure>
+      <MouseToolTip
+          visible={showToolTip}
+          offsetX={15}
+          offsetY={10}
+        >
+          <span className="learn-more">Click to learn more!</span>
+        </MouseToolTip>
     </div>
   );
 }
